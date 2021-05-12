9fe11e91387ec6e9f795d40a45660e1acec99391
Paul T George <68840617+tgpaul@users.noreply.github.com>
2021-05-13 01:56:06 +0530

    Update 3. 2DArray3.c

--- 3. 2DArray3.c
+++ 3. 2DArray3.c
@@ -5,8 +5,7 @@
 */
 
 #include<stdio.h>
-
-const int MAX_SIZE = 10;
+#define MAX_SIZE 20
 
 void main(){
     //Declaring the 2D Array
@@ -14,42 +13,38 @@ void main(){
     int row,col,i,j,item,flag=0;
     
     //Reading the array order
-    printf("Enter the number of rows and columns : \n");
     scanf("%d%d",&row,&col);   
     
     //Reading values for 2D array
-    printf("Enter the values for the 2-Dimensional array :\n");
     for(i=0; i<row; i++) {
         for(j=0;j<col;j++) {
             scanf("%d", &arr[i][j]);
         }
-        printf("\n");
     }
 
+    /*
     //Printing the 2D array
-    printf("The 2-Dimensional array elements are :\n");
     for(i=0; i<row; i++) {
         for(j=0;j<col;j++) {
             printf("%d ", arr[i][j]);
         }
         printf("\n");
     }
+    */
 
     //Read element to be searched for
-    printf("Enter the item to find: ");
     scanf("%d", &item);
     for(i=0; i<row; i++){
         for(j=0;j<col;j++){
             if(arr[i][j] == item){
-        printf("Item found at [%d, %d] \n", i, j);
-        flag++;
+                printf("Item found at [%d, %d] \n", i, j);
+                flag++;
             }
         }
     }
 
     //Element was never found (implies 'flag' was never incremented. Sooooooo...... 'flag' != 0)
     if(flag==0){
-        printf("Item Not found\n");
+        printf("Item not found\n");
     }
-    printf("\nProgram Finished!\n");
 }
